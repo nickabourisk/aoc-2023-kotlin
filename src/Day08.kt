5ab258fbee7c292ea83f0ed6f9636dc74501d797
Nick Abou Risk <nickabourisk@gmail.com>
2023-12-08 10:14:06 +0000

    Day 08 Part 1 solution

---
+++ src/Day08.kt
@@ -0,0 +1,45 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val sequence = input[0]
+
+        val networkMap = (2..<input.size)
+            .map {
+                input[it]
+            }
+            .associate {
+                val (key, left, right) = it.split(" = ", ", ")
+                key to Pair(left.removePrefix("("), right.removeSuffix(")"))
+            }
+
+        val start    = "AAA"
+        val end      = "ZZZ"
+        var location = start
+        var step     = 0
+
+        do {
+            val direction = sequence[step % sequence.length]
+            location = when (direction) {
+                'L'  -> networkMap[location]!!.first
+                'R'  -> networkMap[location]!!.second
+                else -> throw RuntimeException("Sequence should only have 'L' and 'R' directions!")
+            }
+            step++
+        }
+        while (location != end)
+
+        return step
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput1 = readInput("Day08_test1")
+    val testInput2 = readInput("Day08_test2")
+    val input      = readInput("Day08")
+
+    check(part1(testInput1) == 2)
+    check(part1(testInput2) == 6)
+    println("Part1: ${part1(input)}")
+    //println("Part2: ${part2(input)}")
+}
