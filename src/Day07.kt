5bdece44b52d3a34a429c4f034d35b9d31754bb1
Nick Abou Risk <nickabourisk@gmail.com>
2023-12-08 01:28:50 +0000

    Day 07 Part 2 Solution
    also refactored to handle parts 1 and 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,4 +1,4 @@
-enum class Strength {  // example  maxLabel  uniqueLabel
+enum class Strength {  // example  maxOfLabel  uniqueLabel
     FIVE_OF_A_KIND,    // AAAAA       5           1
     FOUR_OF_A_KIND,    // AA8AA       4           2
     FULL_HOUSE,        // 23332       3           2
@@ -8,13 +8,13 @@ enum class Strength {  // example  maxLabel  uniqueLabel
     HIGH_CARD          // 23456       1           5
 }
 
-const val LABELS = "AKQJT98765432"
-fun labelOrdinal(char: Char) = LABELS.indexOf(char)
+class Hand (private val handString: String, private val hasWildcards: Boolean = false): Comparable<Hand> {
+    private val strength = evaluate(handString)
 
-class Hand(private val handString: String): Comparable<Hand> {
-    private val strength: Strength = computeStrength(handString)
+    private fun labelOrdinal(char: Char) =
+        if (hasWildcards) "AKQT98765432J".indexOf(char)
+        else              "AKQJT98765432".indexOf(char)
 
-    override fun toString() = handString
     override fun compareTo(other: Hand): Int {
         var cmp = this.strength.ordinal - other.strength.ordinal
         if (cmp != 0) return cmp
@@ -22,56 +22,53 @@ class Hand(private val handString: String): Comparable<Hand> {
             cmp = labelOrdinal(char) - labelOrdinal(other.handString[i])
             if (cmp != 0) return cmp
         }
-        return cmp
+        return 0 // only reach if both hands are equal
     }
-}
 
-fun computeStrength(handStr: String): Strength {
-    val map = HashMap<Char, Int>()
-    handStr.forEach {
-        map[it] = map.getOrDefault(it, 0) + 1
+    private fun evaluate(handStr: String): Strength {
+        if (handStr == "JJJJJ") return Strength.FIVE_OF_A_KIND // edge-case
+
+        val map = HashMap<Char, Int>()
+        handStr.forEach {
+            if (!hasWildcards || it != 'J')
+                map[it] = map.getOrDefault(it, 0) + 1
+        }
+        val uniqueCount = map.keys.size
+        val maxOfLabel  = map.values.max() + if (hasWildcards) handStr.count { it == 'J' } else 0
+
+        return when {
+            maxOfLabel == 5                     -> Strength.FIVE_OF_A_KIND
+            maxOfLabel == 4                     -> Strength.FOUR_OF_A_KIND
+            maxOfLabel == 1                     -> Strength.HIGH_CARD
+            maxOfLabel == 3 && uniqueCount == 2 -> Strength.FULL_HOUSE
+            maxOfLabel == 3 && uniqueCount == 3 -> Strength.THREE_OF_A_KIND
+            maxOfLabel == 2 && uniqueCount == 3 -> Strength.TWO_PAIR
+            maxOfLabel == 2 && uniqueCount == 4 -> Strength.ONE_PAIR
+            else -> throw RuntimeException("Unhandled hand evaluation!")
+        }
     }
-    val uniqueCount = map.keys.size
-    val maxOfLabel = map.values.max()
-
-    // NGA TODO: replace these with 'when' clauses
-    if (maxOfLabel == 5) return Strength.FIVE_OF_A_KIND
-    if (maxOfLabel == 4) return Strength.FOUR_OF_A_KIND
-    if (maxOfLabel == 1) return Strength.HIGH_CARD
-    if (maxOfLabel == 3) {
-        if      (uniqueCount == 2) return Strength.FULL_HOUSE
-        else if (uniqueCount == 3) return Strength.THREE_OF_A_KIND
-        else throw RuntimeException("Should never get here!")
-    }
-    check(maxOfLabel == 2)
-    if (uniqueCount == 3) return Strength.TWO_PAIR
-    if (uniqueCount == 4) return Strength.ONE_PAIR
-
-    throw RuntimeException("Should never get here!")
 }
 
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun computeWinnings(input: List<String>, hasWildcards: Boolean): Int {
         return input
             .map {
                 val (handTxt, bidTxt) = it.split(" ")
-                Pair(Hand(handTxt), bidTxt.toInt())
+                Pair(Hand(handTxt, hasWildcards), bidTxt.toInt())
             }
             .sortedBy { it.first }
             .reversed()
             .mapIndexed { i, (_, bid) -> (i+1) * bid }
             .sum()
     }
+    fun part1(input: List<String>) = computeWinnings(input, hasWildcards=false)
+    fun part2(input: List<String>) = computeWinnings(input, hasWildcards=true)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test1")
     check(part1(testInput) == 6440)
+    check(part2(testInput) == 5905)
 
     val input = readInput("Day07")
-    part1(input).println()
-    //part2(input).println()
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
