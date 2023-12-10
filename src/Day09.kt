c2242e62b5a3911783d12fdd52378e85b02a5dbc
Nick Abou Risk <nickabourisk@gmail.com>
2023-12-10 17:00:20 +0000

    Day 09 Part 2 solution

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,7 +1,7 @@
 fun computeDiffList(list: List<Long>) = list.windowed(2).map { it[1] - it[0] }
 fun isListAllZeros (list: List<Long>) = list.all { it == 0L }
 
-// TODO: convert this into functional
+// TODO: convert these predict functions into functional
 fun predictNextValue(list: List<Long>): Long {
     val listOfLists = ArrayList<List<Long>>()
 
@@ -14,23 +14,43 @@ fun predictNextValue(list: List<Long>): Long {
     return listOfLists.sumOf { it.last() }
 }
 
+fun predictPreviousValue(list: List<Long>): Long {
+    val listOfLists = ArrayList<List<Long>>()
+
+    listOfLists.add(list)
+    var lastList = list
+    while (!isListAllZeros(lastList)) {
+        lastList = computeDiffList(lastList)
+        listOfLists.add(lastList)
+    }
+    return listOfLists
+                     .reversed()
+                     .map { it.first() }
+                     .reduce { acc, it ->
+                        it - acc
+                     }
+}
+
 fun main() {
     fun part1(input: List<String>): Long {
-        val sequences = ArrayList<List<Long>>()
-        input.forEach { line ->
-            sequences.add(line.split(" ").map { it.toLong() })
+        return input.map { line ->
+            line.split(" ").map { it.toLong() }
         }
-        return sequences.sumOf { predictNextValue(it) }
+        .sumOf { predictNextValue(it) }
     }
 
     fun part2(input: List<String>): Long {
-        return 0L
+        return input.map { line ->
+            line.split(" ").map { it.toLong() }
+        }
+        .sumOf { predictPreviousValue(it) }
     }
 
     val testInput1 = readInput("Day09_test1")
     val input      = readInput("Day09")
     check(part1(testInput1) == 114L)
+    check(part2(testInput1) == 2L)
 
     part1(input).println()
-    //part2(input).println()
+    part2(input).println()
 }
